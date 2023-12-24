b1d439a422dba8cad4968c224161fb4da197b246
AnujKaprawan <anujkp15@gmail.com>
2023-12-24 15:28:28 +0530

    Model Layer Code Added

---
+++ src/main/java/BookMyShow/models/Screen.java
@@ -0,0 +1,27 @@
+package BookMyShow.models;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Screen {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    UUID id;
+    String screenName;
+    @ManyToOne
+    Hall hall;
+    int screenCapacity;
+    boolean status;
+    String type;
+}
